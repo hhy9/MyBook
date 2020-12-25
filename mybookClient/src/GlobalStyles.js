b42220e7ff0856439bb9b8bd9ed7215f2127b723
황혜영 <62981058+hhy9@users.noreply.github.com>
2020-12-25 23:39:04 +0900

    first commit

---
+++ mybookClient/src/GlobalStyles.js
@@ -0,0 +1,17 @@
+import  {createGlobalStyle  }  from 'styled-components';
+
+const GlobalStyled = createGlobalStyle `
+
+  html {
+    width:100%;
+    height:100%;
+  }
+
+  body {
+    width:100%;
+    height:100%;
+  }
+`;
+
+
+export default GlobalStyled
