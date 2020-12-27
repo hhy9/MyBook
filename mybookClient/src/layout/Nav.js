7ea29daf0d02c32e7add6cbbf77b8d70f299c774
황혜영 <62981058+hhy9@users.noreply.github.com>
2020-12-27 19:07:57 +0900

    mainPage+Nav작업 Main폴더구조변경

--- mybookClient/src/layout/Nav.js
+++ mybookClient/src/layout/Nav.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 const Nav = () => {
   return (
     <>
+      <NavDiv>
+          <ul className="nav-ul">
+            <li>
+              책 검색
+            </li>
+          </ul>
+      </NavDiv>  
     </>
   )
 }
+
+const NavDiv = styled.div`
+  .nav-ul {
+    margin:0 auto;
+  }
+`;
 
 export default Nav;
--- mybookClient/src/page/MainPage.js
+++ mybookClient/src/page/MainPage.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import Nav from '../layout/Nav'
 
-const Main = () =>{
+const MainPage = () => {
   return (
     <>
       <MainDiv>
-    <div>  Main Page</div>
-        Main Page
-        Main Page
-        Main Page
-        Main Page
-        Main Page
-        Main Page
-        Main Page
-        Main Page
-        Main Page
-       </MainDiv>
+        <Nav/>
+      </MainDiv>
     </>
   )
 }
@@ -26,4 +18,5 @@ height:100vh;
 background-image: url('https://res.cloudinary.com/tinaland/image/upload/v1596348812/nick-fewings-f2Bi-VBs71M-unsplash_dxjphv.jpg');
 background-size:cover;
 `;
-export default Main;
+
+export default MainPage;
