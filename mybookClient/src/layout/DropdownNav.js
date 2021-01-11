5b77078d7922a50fcd7cbca536f3a39473e36fd1
황혜영 <62981058+hhy9@users.noreply.github.com>
2021-01-12 02:44:57 +0900

    Logo 외 이미지추가

--- mybookClient/src/layout/DropdownNav.js
+++ mybookClient/src/layout/DropdownNav.js
@@ -1,26 +1,49 @@
 import React,{useState} from 'react';
-
-
+import styled from 'styled-components'
+import BookLogo from '../img/BookLogo.jpg'
 const DropdownNav = () => {
   const [open,setOpen] = useState(false)
 
+  
+
   return (
     <>
-    <div>
-      <button onClick={()=>setOpen(!open)}>
-        도서관 & 책
-      </button>
-         {
+    <NavMain>
+      <div>
+        <img className="logoImg" src={BookLogo} alt="Logo" />
+      </div>
+      <div className="firstMenuDiv" onClick={()=>setOpen(!open)}>
+        나의 책       
+        </div>
+         {/* {
            open? (
              <div className="menu"> 
-                <button>도서관 찾기</button>
-                <button>책 찾기</button>
+                <li>도서관 찾기</li>
+                <li>책 찾기</li>
              </div>
            ) : null
-         }
-    </div>
+         } */}
+    </NavMain>
     </>
   )
 }
+
+const NavMain = styled.div`
+  width:40%;
+  margin: 0 auto;
+  
+  .logoImg{
+    
+  }
+  .firstMenuDiv{
+    padding: 15px 30px 0px 30px;
+    color:black;
+    font-weight:bold;
+    font-size:18px;
+  }
+
+`;
+
+
 
 export default DropdownNav;
--- mybookClient/src/page/MainPage.js
+++ mybookClient/src/page/MainPage.js
@@ -17,11 +17,9 @@ const MainPage = () => {
 const MainDiv = styled.div`
 width:100%;
 height:100vh;
-background-image: url('https://res.cloudinary.com/tinaland/image/upload/v1596348812/nick-fewings-f2Bi-VBs71M-unsplash_dxjphv.jpg');
-background-size:cover;
   .main-header{
     height:8%;
-    border-bottom:1px solid white;
+   
   }
 `;
 
